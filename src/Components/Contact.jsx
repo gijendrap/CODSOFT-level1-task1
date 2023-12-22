8f7e8a82d83388c712de8a3e02f3ec254265e3f4
gijendrap <vallalarenhiravallal@gmail.com>
2023-12-22 08:39:55 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -20,6 +20,7 @@ const Contact = () => {
   };
 
   return (
+    <div>
     <div className="contact" id="contact">
       <div className="footer-left">
         <div className="login">
@@ -91,6 +92,13 @@ const Contact = () => {
           </span>
         </div>
       </div>
+    </div>
+      <button className="Btn" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+      <svg height="1.2em" className="arrow" viewBox="0 0 512 512">
+        <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
+      </svg>
+      <p className="text">Back to Top</p>
+    </button>
     </div>
   );
 };

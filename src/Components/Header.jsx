c0d8efbf9805fb5c49b50105fe8935ba12441cbb
gijendrap <vallalarenhiravallal@gmail.com>
2023-12-26 13:51:43 +0530

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import '../Css/Header.css'; 
 import logo from '../assets/gijendra.png';
 
@@ -20,30 +18,27 @@ const Header = () => {
           alt="Your Logo Alt Text"
           width="200"
           height="50"
-          style={{position:'absolute',top:'6px'}}
+          style={{ position: 'absolute', top: '6px' }}
           className="d-inline-block align-top"
         />
       </Navbar.Brand>
-      <Navbar.Toggle 
-        aria-controls="navbarScroll" 
-        onClick={handleToggle} 
-      >
-        <FontAwesomeIcon 
-          icon={isNavbarExpanded ? faTimes : faBars} 
-          style={{ color: 'black' }} 
-        />
-      </Navbar.Toggle>
-      <Navbar.Collapse 
-        id="navbarScroll" 
-        onEnter={() => setIsNavbarExpanded(true)} 
+      <div className={`toggle ${isNavbarExpanded ? 'rotate' : ''}`} onClick={handleToggle}>
+        <div className="bars" id="bar1"></div>
+        <div className="bars" id="bar2"></div>
+        <div className="bars" id="bar3"></div>
+      </div>
+      <Navbar.Collapse
+        id="navbarScroll"
+        in={isNavbarExpanded}
+        onEnter={() => setIsNavbarExpanded(true)}
         onExit={() => setIsNavbarExpanded(false)}
       >
         <Nav className="mr-auto">
-          <Nav.Link style={{color:'black', fontFamily:'monospace'}} href="#About">About</Nav.Link>
-          <Nav.Link style={{color:'black', fontFamily:'monospace'}} href="#projects">Projects</Nav.Link>
-          <Nav.Link style={{color:'black', fontFamily:'monospace'}} href="#skills-section">Skills</Nav.Link>
-          <Nav.Link style={{color:'black', fontFamily:'monospace'}} href="#contact">Contact</Nav.Link>
-          <Nav.Link style={{color:'black', fontFamily:'monospace'}} href="https://drive.google.com/file/d/1nUEAcGu9lj5Yi2kVm0V_Ne5d5hZYYbvY/view?usp=sharing" target="_blank" rel="noreferrer">Resume</Nav.Link>
+          <Nav.Link style={{ color: 'black', fontFamily: 'monospace' }} href="#About">About</Nav.Link>
+          <Nav.Link style={{ color: 'black', fontFamily: 'monospace' }} href="#projects">Projects</Nav.Link>
+          <Nav.Link style={{ color: 'black', fontFamily: 'monospace' }} href="#skills-section">Skills</Nav.Link>
+          <Nav.Link style={{ color: 'black', fontFamily: 'monospace' }} href="#contact">Contact</Nav.Link>
+          <Nav.Link style={{ color: 'black', fontFamily: 'monospace' }} href="https://drive.google.com/file/d/1nUEAcGu9lj5Yi2kVm0V_Ne5d5hZYYbvY/view?usp=sharing" target="_blank" rel="noreferrer">Resume</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
